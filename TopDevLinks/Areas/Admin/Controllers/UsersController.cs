66a4ffe52d6b089d651524c06c1ec1a3ed7d922d
Jef Claes <jef.claes@gmail.com>
2012-03-04 19:36:09 +0000

    Removed razor rendering in view, this will be done using backbone

--- TopDevLinks/Areas/Admin/Controllers/UsersController.cs
+++ TopDevLinks/Areas/Admin/Controllers/UsersController.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using TopDevLinks.Infrastructure;
-using TopDevLinks.Areas.Admin.Models.ViewModels;
 using TopDevLinks.Queries;
 
 namespace TopDevLinks.Areas.Admin.Controllers
@@ -13,13 +12,7 @@ namespace TopDevLinks.Areas.Admin.Controllers
     public class UsersController : MongoContextController
     {
         public ActionResult Index()
-        {          
-            ViewData.Model = new UsersIndexViewModel()
-            {
-                Users = Execute(new GetAllUsersQuery())
-                    .Select(u => new UserIndexViewModel(u.Login, u.Email))
-            };
-
+        {         
             return View();
         }
     }
