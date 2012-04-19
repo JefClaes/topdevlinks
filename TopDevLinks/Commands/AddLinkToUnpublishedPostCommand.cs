ffc9ad8f2fa025b6b66f1136b8ebefbc63028993
Jef Claes <jef.claes@gmail.com>
2012-04-19 07:21:38 +0100

    Added AddLinkToUnpublishedPostCommand. Not under test yet.

---
+++ TopDevLinks/Commands/AddLinkToUnpublishedPostCommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TopDevLinks.Infrastructure;
+using TopDevLinks.Models.Entities;
+using MongoDB.Driver.Builders;
+
+namespace TopDevLinks.Commands
+{
+    public class AddLinkToUnpublishedPostCommand : Command
+    {
+        private Link _link;
+
+        public AddLinkToUnpublishedPostCommand(Link link)
+        {
+            _link = link;
+        }
+
+        public override void Execute()
+        {
+            var unpublishedPosts = MongoContext.GetCollection<Post>().Find(Query.EQ("Published", false));
+
+            foreach (var unpublishedPost in unpublishedPosts)            
+                unpublishedPost.AddLink(_link);            
+        }
+    }
+}
