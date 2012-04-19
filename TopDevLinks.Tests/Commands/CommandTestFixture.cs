1d629219e3dd8192850dc12d8615bd6500425d89
Jef Claes <jef.claes@gmail.com>
2012-04-19 17:20:31 +0100

    Added CommandTestFixture

---
+++ TopDevLinks.Tests/Commands/CommandTestFixture.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TopDevLinks.Infrastructure;
+
+namespace TopDevLinks.Tests.Commands
+{    
+   public abstract class CommandTestFixture
+    {
+        protected MongoContext MongoContext { get; private set; }
+        protected EntityStore EntityStore { get; private set; }
+
+        protected CommandTestFixture()
+        {
+            MongoContext = new MongoContext();
+            EntityStore = new EntityStore(MongoContext);
+        }
+
+        protected void Execute(Command command)
+        {
+            command.MongoContext = MongoContext;
+            command.Execute();
+        }
+    }
+}
