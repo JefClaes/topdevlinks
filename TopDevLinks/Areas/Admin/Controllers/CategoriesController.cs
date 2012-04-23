e2175bea7281356a619455c2c39f9441b2558688
Jef Claes <jef.claes@gmail.com>
2012-04-23 07:01:33 +0100

    Added categories view and controller

---
+++ TopDevLinks/Areas/Admin/Controllers/CategoriesController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TopDevLinks.Areas.Admin.Controllers
+{
+    public class CategoriesController : Controller
+    {       
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
