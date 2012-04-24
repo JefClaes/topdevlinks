c217fc776d93a7b9743f17af54b985a648261663
Jef Claes <jef.claes@gmail.com>
2012-04-24 06:58:47 +0100

    Added categories index

--- TopDevLinks/Areas/Admin/Controllers/CategoriesController.cs
+++ TopDevLinks/Areas/Admin/Controllers/CategoriesController.cs
@@ -3,13 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TopDevLinks.Areas.Admin.Models.ViewModels;
+using TopDevLinks.Infrastructure;
+using TopDevLinks.Queries;
 
 namespace TopDevLinks.Areas.Admin.Controllers
 {
-    public class CategoriesController : Controller
+    public class CategoriesController : MongoContextController
     {       
         public ActionResult Index()
         {
+            ViewData.Model = new CategoriesIndexViewModel() { Categories = Execute(new GetCategoriesQuery()) };
+
             return View();
         }
     }
---
+++ TopDevLinks/Areas/Admin/Models/ViewModels/CategoriesIndexViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TopDevLinks.Areas.Admin.Models.ViewModels
+{
+    public class CategoriesIndexViewModel
+    {
+        public CategoriesViewModel Categories { get; set; }
+
+        public string Name { get; set; }
+    }
+}
