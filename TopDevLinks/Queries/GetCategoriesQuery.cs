dcb1b708112aeeac1f648f6f66a343eaacddb58b
Jef Claes <jef.claes@gmail.com>
2012-04-24 06:51:23 +0100

    Added GetCategories Query

---
+++ TopDevLinks/Queries/GetCategoriesQuery.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TopDevLinks.Areas.Admin.Models.ViewModels;
+using TopDevLinks.Infrastructure;
+using TopDevLinks.Models.Entities;
+
+namespace TopDevLinks.Queries
+{
+    public class GetCategoriesQuery : Query<CategoriesViewModel>
+    {
+        public override CategoriesViewModel Execute()
+        {
+            var items = MongoContext
+                .GetCollection<Category>()
+                .FindAll()
+                .Select(c => new CategoryViewModel(c.Id.ToString(), c.Name));
+
+            return new CategoriesViewModel(items);
+        }
+    }
+}
